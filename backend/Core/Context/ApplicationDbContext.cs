147cf0704eaf64090f7b9b8b9238fed0cce8e74a
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-04-11 10:33:09 -0700

    Add project files.

---
+++ backend/Core/Context/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using backend.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Core.Context
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+		}
+
+        public DbSet<Company> Companies { get; set; }
+		public DbSet<Job> Jobs { get; set; }
+		public DbSet<Candidate> Candidates { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			/* each job has one company entity, each company has many
+			 * jobs, and each job has a corresponding company id */
+			modelBuilder.Entity<Job>()
+				.HasOne(job => job.Company)
+				.WithMany(company => company.Jobs)
+				.HasForeignKey(job => job.CompanyId);
+
+			/* each candidate has one job, each job has many candidates,
+			 * and each candidate has an associated job id */
+			modelBuilder.Entity<Candidate>()
+				.HasOne(candidate => candidate.Job)
+				.WithMany(job => job.Candidates)
+				.HasForeignKey(candidate => candidate.JobId);
+		}
+	}
+}
